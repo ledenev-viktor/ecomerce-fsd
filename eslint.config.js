a349f7ede42920ee3342436eb2631b958b97c22f
ledenev-viktor <ledenev4252@gmail.com>
2024-08-14 05:29:38 +0300

    added gh actions, pre-commit, update vite.config, update eslint.config, added libs

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,40 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import * as emotion from '@emotion/eslint-plugin';
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import react from '@eslint-react/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
-  files: ['**/*.{ts,tsx}'],
-  ignores: ['dist', 'coverage'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      '@typescript-eslint': tseslint,
+      react,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': "error",
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
   },
-  plugins: {
-    'react-hooks': reactHooks,
-    'react-refresh': reactRefresh,
-    '@emotion': emotion,
+  {
+    ignores: ['dist', 'coverage'],
   },
-  rules: {
-    ...reactHooks.configs.recommended.rules,
-    '@emotion/jsx-import': 'error',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-})
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
-import path from "node:path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
-  },
-  server: {
-    port: 3000,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+    ],
+    resolve: {
+      alias: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
+    },
+    server: {
+      port: 3000,
+    },
+    base: mode === 'development' ? '/' : '/ecomerce-fsd/',
+  };
 });
